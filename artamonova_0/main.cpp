cdfe41f9040ab9195f44e6c1936de1287dbc987d
Pugovvka <163815597+Pugovvka@users.noreply.github.com>
2025-09-12 00:17:22 +0300

    добавлен вывод hello world и запрос символа

---
+++ artamonova_0/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    char inputChar;
+
+    std::cout << "Hello World!\n";
+    std::cout << "Vvedite simvol: ";
+    std::cin >> inputChar;
+
+    return 0;
+}
